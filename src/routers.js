627bcac6b44b0d577427fd8839d1699acf1ef44f
mribeirogabriel <mribeirogabriel@hotmail.com>
2019-12-06 06:06:41 -0300

    node-api

---
+++ src/routers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routers = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+routers.get('/products', ProductController.index);
+routers.get('/products/:id',ProductController.show)
+routers.post('/products', ProductController.store);
+routers.put('/products/:id', ProductController.update)
+routers.delete('/products/:id', ProductController.destroy)
+
+module.exports = routers;
